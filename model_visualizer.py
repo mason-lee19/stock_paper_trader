af2b72b845a4738c655d77940e152a180b7660a3
Mason Lee <mason.leewp@gmail.com>
2024-04-22 13:00:11 -0700

    Finalized model visualizer

--- model_visualizer.py
+++ model_visualizer.py
@@ -1,39 +1,108 @@
 import argparse
 import pandas as pd
+import neat
+import pickle
+import os
 
-from utils import DataHandler, ApplyIndicators
+from utils import DataHandler, ApplyIndicators, Plotter
 
-def prepare_data(dataMgr) -> pd.DataFrame:
-    # Query data
-    df = dataMgr.query_crypto_data()
-    df = dataMgr.drop_cols(df)
+class TradeSimulation:
 
-    # Apply techincal analysis
-    indMgr = ApplyIndicators()
-    df = indMgr.apply_ta(df)
+    def __init__(self):
+        self.df = pd.DataFrame()
+        self.close_prices = pd.DataFrame()
 
-    # Normalize 
-    cols_to_exclude = ['symbol','timestamp']
-    excluded_columns_df = df[cols_to_exclude]
+        self.config = self.get_config()
 
-    cols_to_normalize = df.drop(cols_to_exclude,axis=1).columns
-    
-    normalized_data = df[cols_to_normalize].apply(dataMgr.normalize)
+        self.results = []
 
-    df_normalize = pd.concat([excluded_columns_df,normalized_data],axis=1)
+    def prepare_data(self,dataMgr) -> None:
+        # Query data
+        df = dataMgr.query_crypto_data()
+        df = dataMgr.drop_cols(df)
 
-    df_normalize.dropna(inplace=True)
+        # Apply techincal analysis
+        indMgr = ApplyIndicators()
+        df = indMgr.apply_ta(df)
 
-    return df_normalize
+        # Normalize 
+        cols_to_exclude = ['symbol','timestamp']
+        excluded_columns_df = df[cols_to_exclude]
+
+        self.close_prices = df['close']
+
+        cols_to_normalize = df.drop(cols_to_exclude,axis=1).columns
+        
+        normalized_data = df[cols_to_normalize].apply(dataMgr.normalize)
+
+        df_normalize = pd.concat([excluded_columns_df,normalized_data],axis=1)
+
+        df_normalize.dropna(inplace=True)
+
+        self.df = df_normalize
+
+    def get_config(self):
+        local_dir = os.path.dirname(__file__)
+        config_path = os.path.join(local_dir,'config.txt')
+
+        config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, 
+                            neat.DefaultSpeciesSet, neat.DefaultReproduction, config_path)
+
+        return config
+
+
+    def test_model(self,model_name):
+        with open(model_name,"rb") as f:
+            winner = pickle.load(f)
+        winner_net = neat.nn.FeedForwardNetwork.create(winner,self.config)
+
+        self.trade(winner_net)
+
+    def trade(self,net):
+        start_index = 30
+        buy_signal = False
+
+        for idx in range(start_index,len(self.df)):
+            inputs = (buy_signal,
+                      self.df['open'].iloc[idx],
+                      self.df['high'].iloc[idx],
+                      self.df['low'].iloc[idx],
+                      self.df['close'].iloc[idx],
+                      self.df['volume'].iloc[idx],
+                      self.df['SMA_20'].iloc[idx],
+                      self.df['SMA_50'].iloc[idx],
+                      self.df['EMA_20'].iloc[idx],
+                      self.df['RSI_14'].iloc[idx],
+                      self.df['MACD'].iloc[idx],
+                      self.df['MACD_Signal'].iloc[idx],
+                      self.df['BB_Upper'].iloc[idx],
+                      self.df['BB_Middle'].iloc[idx],
+                      self.df['BB_Lower'].iloc[idx],
+                      self.df['ATR_14'].iloc[idx],
+                      self.df['OBV'].iloc[idx],
+                      self.df['ADL'].iloc[idx])
+            
+            output = net.activate(inputs)
+            decision = output.index(max(output))
+
+            if decision == 0 and not buy_signal:
+                buy_signal = True
+                self.results.append(('BUY',idx))
+            elif decision == 1 and buy_signal:
+                buy_signal = False
+                self.results.append(('SELL',idx))
 
 def main(args):
     # Get api key and secret
     dataMgr = DataHandler(requestConfig=args)
 
     # Pull and prepare data to be analyzed by model
-    df = prepare_data(dataMgr)
+    simu = TradeSimulation()
+    simu.prepare_data(dataMgr)
 
-    print(df.iloc[:10])
+    simu.test_model(args.model)
+
+    plot = Plotter(simu.results,simu.close_prices)
 
 
 if __name__ == "__main__":
@@ -41,6 +110,7 @@ if __name__ == "__main__":
 
     parser.add_argument("-s","--stockTicker",type=str,default=None,help="stock ticker")
     parser.add_argument("-d","--startDate",type=str,default="2020-01-01",help="start date")
+    parser.add_argument("-m","--model",type=str,default="Models/ModelA.pickle",help="Models/ModelA.pickle")
 
     args = parser.parse_args()
 
--- utils/plotter.py
+++ utils/plotter.py
@@ -1,5 +1,38 @@
 import matplotlib.pyplot as plt
 
 class Plotter:
-    def __init__(self):
-        pass
+    def __init__(self,results,close_prices):
+        self.df = close_prices
+
+        self.buy_indices = [idx for action,idx in results if action=='BUY']
+        self.buy_prices = [close_prices.iloc[idx] for idx in self.buy_indices]
+        self.sell_indices = [idx for action,idx in results if action=='SELL']
+        self.sell_prices = [close_prices.iloc[idx] for idx in self.sell_indices]
+
+        self.plot()
+
+    def plot(self):
+        plt.plot(self.df,label='Close')
+
+        plt.scatter(self.buy_indices,self.buy_prices,color="green",
+                    marker='^',label='Buy')
+        plt.scatter(self.sell_indices,self.sell_prices,color='red',
+                    marker='v',label='Sell')
+        
+        plt.xlabel('Index')
+        plt.ylabel('Price')
+
+        plt.title(f'Model Signal BackTest, percent made: {self.calc_percent()}')
+
+        plt.legend()
+
+        plt.show()
+
+    def calc_percent(self) -> int:
+        total_percent_change = 0
+
+        for buy_price, sell_price in zip(self.buy_prices,self.sell_prices):
+            percent_change = ((sell_price-buy_price)/buy_price) * 100
+            total_percent_change += percent_change
+        
+        return total_percent_change
